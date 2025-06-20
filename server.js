6bb6f32f2782601cda9fa5e3ed406d684b20e3cd
Tinku-137 <savaram.praveenkumar@gmail.com>
2025-06-20 16:04:48 +0530

    Removed duplicate express import

--- server.js
+++ server.js
@@ -1,10 +1,3 @@
-const express = require("express");
-const cors = require("cors");
-const nodemailer = require("nodemailer");
-require("dotenv").config();
-
-const app = express();
-const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
